28e40eefce6b7815da2c45f5bdd4d5d88f55a28d
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2025-06-18 13:30:28 -0600

    Add files via upload

---
+++ config/services.js
@@ -0,0 +1,8 @@
+const services = [
+    { name: "auth", url: "https://adriauth.onrender.com", path: "/api1" },
+    { name: "server1", url: "https://adriasistencia.onrender.com", path: "/api2/asistencia" },
+    { name: "server2", url: "https://adriacademic.onrender.com", path: "/api3/academia" },
+
+  ];
+  
+  module.exports = { services };
